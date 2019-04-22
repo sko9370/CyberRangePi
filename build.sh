25e8c5fce9824f2fcb7a2efcdbc25607badc85e5
sko9370 <sko9370@ymail.com>
2019-04-21 23:30:29 -0400

    build.sh

---
+++ build.sh
@@ -0,0 +1,42 @@
+echo building images
+
+cd ../p00-warmup
+docker build -t p00 . > /dev/null
+
+cd ../p01-firmware
+docker build -t p01 . > /dev/null
+
+cd ../p02-sandbox
+docker build -t p02 . > /dev/null
+
+cd ../p03-imagemanipulator
+docker build -t p03 . > /dev/null
+
+cd ../p04-coderunner
+docker build -t p04 . > /dev/null
+
+cd ../p05-crypto
+docker build -t p05 . > /dev/null
+
+cd ../p06-hades
+docker build -t p06 . > /dev/null
+
+cd ../p07-exifreader
+docker build -t p07 . > /dev/null
+
+cd ../p08-hackme
+docker build -t p08 . > /dev/null
+
+cd ../p09-poseidon
+docker build -t p09 . > /dev/null
+
+cd ../p10-assignment
+docker build -t p10 . > /dev/null
+
+cd ../p11-zeus
+docker build -t p11 .
+
+cd ../../docker-swarm-visualizer
+docker build -t viz:latest .
+
+echo !!!!!!!!!!!!!! Please wait until the builds finish !!!!!!!!!!!!!!!
