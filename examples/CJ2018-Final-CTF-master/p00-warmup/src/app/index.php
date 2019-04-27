36a0c7f599fed02ea4ae92d41516210647d8a61f
sko9370 <sko9370@ymail.com>
2019-04-27 10:54:49 -0400

    reorgy

--- examples/CJ2018-Final-CTF-master/p00-warmup/src/app/index.php
+++
@@ -1,8 +0,0 @@
- <?php
-  if (!empty($_GET['number'])) {
-    $number = $_GET['number'];
-    $is_numeric = (is_numeric($number) ? "a number" : "not a number");
-    print eval("print '$number is $is_numeric';");
-  } else {
-    highlight_file(__FILE__);
-  }
--- examples/CJ2018-Final-CTF-master/p03-imagemanipulator/src/app/inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define('CONVERT_PATH', '/usr/bin/convert');
-define('TEMPLATE_DIR', 'Template/');
